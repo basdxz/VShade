c69f9272171c73465c06bad25aa581ebd2de9095
basdxz <tudurap.com@gmail.com>
2022-05-14 17:37:28 +0100

    Added program interface for shader query stuff

--- src/main/java/com/basdxz/vshade/query/newstuff/IQueryProgramInterface.java
+++ src/main/java/com/basdxz/vshade/query/newstuff/IQueryProgramInterface.java
@@ -1,9 +1,12 @@
 package com.basdxz.vshade.query.newstuff;
 
 import java.nio.IntBuffer;
+import java.util.List;
 
 public interface IQueryProgramInterface {
     int interfaceType();
+
+    List<IQueryProgramResource> resources();
 
     IntBuffer properties();
 }
